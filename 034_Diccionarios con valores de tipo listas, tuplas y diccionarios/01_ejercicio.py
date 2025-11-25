539c443c3dfe88d3a72cd067aeb7ec2e1f2a6e51
wilson-j-insaurralde <wilson-insaurralde@hotmail.com>
2025-11-25 11:19:09 -0300

    Diccionarios: con valores de tipo listas, tuplas y diccionarios

---
+++ 034_Diccionarios con valores de tipo listas, tuplas y diccionarios/01_ejercicio.py
@@ -0,0 +1,40 @@
+"""
+Confeccionar un programa que permita cargar un código de producto como clave en un diccionario. Guardar para dicha clave el nombre del producto, su precio y cantidad en stock.
+Implementar las siguientes actividades:
+1) Carga de datos en el diccionario.
+2) Listado completo de productos.
+3) Consulta de un producto por su clave, mostrar el nombre, precio y stock.
+4) Listado de todos los productos que tengan un stock con valor cero.
+"""
+
+def cargar():
+    productos = {}
+    seguir = "s"
+    while seguir=="s":
+        codigo=int(input("ingrese el codigo del producto: "))
+        descripcion=(input("ingrese la descripcion del producto: "))
+        precio=int(input("ingrese el precio del producto: "))
+        stock=int(input("ingrese el stock: "))
+        productos[codigo]=(descripcion,precio,stock)
+        seguir=input("de sea seguir ingresando productos? [s/n]")
+
+    return productos
+def listacompleta(productos):
+    print("lista de productos: ")
+    for producto in productos:
+        print (producto,productos[producto][0],productos[producto][1],productos[producto][2],sep=" --- ")
+def consultaproducto(productos):
+    ver=int(input("ingrese el codigo del producto a consultar: "))
+    if ver in productos: 
+        print("el producto es: ")
+        print(ver,productos[ver][0],productos[ver][1],productos[ver][2], sep=" --- ")
+def listado_stock_cero(productos):
+    print("Listado de articulos con stock en cero:")
+    for codigo in productos:
+        if productos[codigo][2]==0:
+            print(codigo,productos[codigo][0],productos[codigo][1],productos[codigo][2])
+
+productos=cargar()
+listacompleta(productos)
+consultaproducto(productos)
+listado_stock_cero(productos)
---
+++ 034_Diccionarios con valores de tipo listas, tuplas y diccionarios/03_ejercicio.py
@@ -0,0 +1,43 @@
+"""
+Se desea almacenar los datos de 3 alumnos. Definir un diccionario cuya clave sea el número de documento del alumno. Como valor almacenar una lista con componentes de tipo tupla donde almacenamos nombre de materia y su nota.
+Crear las siguientes funciones:
+1) Carga de los alumnos (de cada alumno solicitar su dni y los nombres de las materias y sus notas)
+2) Listado de todos los alumnos con sus notas
+3) Consulta de un alumno por su dni, mostrar las materias que cursa y sus notas.
+"""
+
+def cargar():
+    alumnos={}
+    for x in range(3):
+        dni=int(input("Ingrese el numero de dni:"))
+        listamaterias=[]
+        continua="s"
+        while continua=="s":
+            materia=input("Ingrese el nombre de materia que cursa:")
+            nota=int(input("Ingrese la nota:"))
+            listamaterias.append((materia,nota))
+            continua=input("Desea cargar otra materia para dicho alumno [s/n}:")
+        alumnos[dni]=listamaterias
+    return alumnos
+
+
+def listar(alumnos):
+    for dni in alumnos:
+        print("Dni del alumno",dni)
+        print("Materias que cursa y notas")
+        for nota,materia in alumnos[dni]:
+            print(materia,nota)
+
+
+def consulta_notas(alumnos):
+    dni=int(input("Ingrese el dni a consultar:"))
+    if dni in alumnos:
+        for nota,materia in alumnos[dni]:
+            print(materia,nota)
+
+
+
+
+alumnos=cargar()
+listar(alumnos)
+consulta_notas(alumnos)  

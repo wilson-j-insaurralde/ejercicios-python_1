964d1f01c7b4b71eec6d8cb2d24de64ea2fa79cb
wilson-j-insaurralde <wilson-insaurralde@hotmail.com>
2025-12-20 17:33:55 -0300

    .

---
+++ 072_tkinter control Canvas/01_ejercicio.py
@@ -0,0 +1,6 @@
+"""
+Autor: Wilson J. Insaurralde
+Email: wilson-insaurralde[arroba]hotmail[punto]com
+Derechos de Autor (c) 2025 Wilson J. Insaurralde. Todos los derechos reservados.
+"""
+"""Confeccionar un programa que cree un objeto de la clase Canvas y muestre la funcionalidad de las principales primitivas gráficas."""

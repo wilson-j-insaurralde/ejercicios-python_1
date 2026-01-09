9842bacf0a8a73a7c9e9e7911d8193ebda1acc5a
wilson-j-insaurralde <wilson-insaurralde@hotmail.com>
2026-01-09 16:33:00 -0300

    resolviendo ejercicios

---
+++ 090_Recursividad Problemas donde conviene aplicar la recursividad/01_ejercicio.py
@@ -0,0 +1,7 @@
+"""
+Autor: Wilson J. Insaurralde
+Email: wilson-insaurralde[arroba]hotmail[punto]com
+Derechos de Autor (c) 2025 Wilson J. Insaurralde. Todos los derechos reservados.
+"""
+"""Recorrer un árbol de directorios en forma recursiva. Mostrar de cada directorios los archivos y directorios, luego descender a cada directorio y hacer la misma actividad."""
+

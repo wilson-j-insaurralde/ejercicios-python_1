3e9e13599818ffdb47901768c957ec433bf35779
wilson-j-insaurralde <wilson-insaurralde@hotmail.com>
2026-01-13 16:17:15 -0300

    Funciones lambda en Python

--- 092_Funciones lambda en Python/03_ejercicio.py
+++ 092_Funciones lambda en Python/03_ejercicio.py
@@ -18,3 +18,27 @@ Un String solo con las vocales
 Un String solo con los caracteres en minúsculas
 Un String con todos los caracteres no alfabéticos
 """
+
+def filtrar(cadena,fn):
+    cad=""
+    for caracter in cadena:
+        if fn(caracter):
+            cad=cad+caracter
+    return cad
+
+cadena="¿Esto es la prueba 1 o la prueba 2?"
+print("String original")
+print(cadena)
+print("String solo con las vocales")
+cadena2=filtrar(cadena, lambda car: car == 'a' or car == 'e' or car == 'i' or car == 'o' or car == 'u' 
+                                    or car == 'A' or car == 'E' or car == 'I' or car == 'O' or car == 'U')
+print(cadena2)
+
+
+print("String solo con los caracteres en minúscula")
+cadena3=filtrar(cadena, lambda car: car >='a' and car <= 'z')
+print(cadena3)
+
+print("String solo con los caracteres no alfabéticos")
+cadena3=filtrar(cadena, lambda car: not(car >='a' and car <= 'z') and not(car >='A' and car <= 'Z'))
+print(cadena3)
